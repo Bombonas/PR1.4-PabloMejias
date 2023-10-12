63d65a71f7d65fea8ed831141321d11c5e0078e0
Pablo Mejías <pablomejiast@gmail.com>
2023-10-12 11:01:28 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws InterruptedException, IOException {
+        boolean running = true;
+        while (running) {
+            String menu = "Escull una opció:";
+            menu = menu + "\n 0) PR130mainPersonesHashmap";
+            menu = menu + "\n 1) PR131hashmap";
+            menu = menu + "\n 2) PR131mainEscriu";
+            // Adapta aquí les altres classes de l’exercici (PR122cat…)
+            menu = menu + "\n 100) Sortir";
+            System.out.println(menu);
+    
+    
+            int opcio = Integer.valueOf(llegirLinia("Opció:"));
+            try {
+            switch (opcio) {
+                case 0: break;
+                case 1: break;
+                case 2: break;
+                // Adapta aquí les altres classes de l’exercici (PR122cat…)
+                case 100: running = false; break;
+                default: break;
+            }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        in.close();  
+    }
+
+    static public String llegirLinia (String text) {
+        System.out.print(text);
+        return in.nextLine();
+    }
+}
